83b72145e75ddf61829118b12deb0c94d16add71
papsoking <papefadiaba728@gmail.com>
2025-02-27 13:20:06 +0000

    Lien frontend

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "https://red-product-backend-a1ix.onrender.com",
+    "https://red-product-frontend.onrender.com",
     "https://red-product-ibrahima-fadiabas-projects.vercel.app",
   ],
   credentials: true,
